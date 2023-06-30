ea82fad63eb71f8ea01bd4f9813c15e46554c8c6
marcelo.gil <marceloalgil@gmail.com>
2023-06-30 12:18:12 -0300

    variables de entorno

--- src/config/config.js
+++ src/config/config.js
@@ -4,6 +4,8 @@ dotenv.config();
 
 export default {
     persistence: process.env.PERSISTENCE,
-    mongoUrl: process.env.MONGOURL,
-    secrets: process.env.SECRETS
+    mongoUrl: process.env.MONGO_URL,
+    secrets: process.env.SECRETS,
+    adminPassword: process.env.ADMIN_PASSWORD,
+    adminEmail: process.env.ADMIN_EMAIL
 }
